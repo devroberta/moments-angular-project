0a98a61c9f66a30b81118295acb017d8c516d437
Roberta de Siqueira <designer.roberta@gmail.com>
2024-02-01 22:05:20 -0300

    service list

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { Animal } from 'src/app/interface/Animal';
 
+import { ListService } from 'src/app/service/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -18,8 +20,16 @@ export class ListRenderComponent {
 
   animalDetails: string = '';
 
+  constructor(private listService: ListService) {
+
+  }
+
   showAge(animal: Animal): void {
     alert(`O animal tem: ${animal.age} anos`);
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+  }
+
+  removeAnimal(animal: Animal) {
+    this.animals = this.listService.remove(this.animals, animal);
   }
 }
